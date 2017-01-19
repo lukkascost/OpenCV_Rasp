073b8345034671d4179b5e670d83bf0dc44e49cb
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-01-19 10:40:30 -0300

    Arquivo para testar os resultados do Prof. Pedrosa

---
+++ svm_tudo.py
@@ -0,0 +1,8 @@
+
+from Metodos import *
+address = "DataBase/SVM_Vectors.txt"
+bdTeste = ler_arquivo("DataBase/training.dat")
+bdTreino = ler_arquivo("DataBase/testing.dat")
+
+
+print bdTeste[0],"\n", bdTreino[0]
