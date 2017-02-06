7dde06d182fbe687144d49eaab90c7363b0579bc
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-02-06 13:44:21 -0300

    update

--- GLCM.py
+++ GLCM.py
@@ -2,8 +2,18 @@
 import cv2
 import numpy as np
 
+def getCoOccurrenceMatrix(image, grayscale):
+    coOccurence = np.zeros((grayscale,grayscale))
+    for i in range(len(image)):
+        for j in range(len(image[0])):
+            coOccurence[image[i][j]][image[i][j+1]]= coOccurence[image[i][j]][image[i][j+1]] +1
+    return coOccurence 
+
+
 # Load an color image in grayscale
 img = cv2.imread('DataBase/c1_1.JPG',0)
 imgQuantized = img.copy()
 
-print img, imgQuantized
+coOccurence = getCoOccurrenceMatrix(imgQuantized, 256)
+
+print coOccurence
