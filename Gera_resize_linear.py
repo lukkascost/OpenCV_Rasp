ebc13e5cef779ecf55b755eaccdd38cc21e6de17
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-03-13 13:05:11 -0300

    44 a 71 lanczos4

--- Gera_resize_linear.py
+++ Gera_resize_linear.py
@@ -2,8 +2,5 @@ import thread
 from Metodos import *
 percent = 90
 
-for i in range(40,101):
-    thread.start_new_thread( cria_Arquivo_GLCM, ((float(i)/100),0,"INTER_NEAREST"))
-    thread.start_new_thread( cria_Arquivo_GLCM, ((float(i)/100),2,"INTER_AREA"))
-    thread.start_new_thread( cria_Arquivo_GLCM, ((float(i)/100),3,"INTER_CUBIC"))
+for i in range(44,101):
     cria_Arquivo_GLCM((float(i)/100),4,"INTER_LANCZOS4")
