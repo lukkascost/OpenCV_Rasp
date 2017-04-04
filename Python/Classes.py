d8cb0ff4ae2a36bedbb36007a93609c4dda5afc0
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-04-04 18:21:21 -0300

    implementado classes

--- Python/Classes.py
+++ Python/Classes.py
@@ -1,55 +1,111 @@
+# -*- coding: cp1252 -*-
 ##################################################################################################################################################################################################
 from Metodos import *
+import pickle as pk
 import copy
 ##################################################################################################################################################################################################
-
-
-
+## CUIDA DE CADA ITERAÇÃO NA EXECUCAO DE TREINO-TESTE
 class iteracao(object):
-        def __init__(self,nclasses):
+        ## CONSTRUTOR DA CLASSE INIT
+        ## nclasses: NUMERO DE CLASSES QUE O CLASSIFICADOR POSSUI.
+        ## nTeste: NUMERO DE AMOSTRAS PARA TESTE.
+        def __init__(self,nclasses, nTeste):
                 self.dados = np.zeros((nclasses,nclasses))
                 self.escore_erro = np.zeros((nclasses,1))
                 self.escore_acerto = np.zeros((nclasses,1))
+                self.nclasses = nclasses
+                self.conj_treino = ""
+                self.conj_treino_label = ""
+                self.conj_teste = ""
+                self.conj_teste_label = ""
+                self.nTeste = nTeste
+                svm_params = ""
+        ##################################################################################################################################################################################################
+        ## ENCAPSULAMENTO DA VARIAVEL dados
+        ## conf_mat UMA MATRIZ NUMPY nclasses X nclasses CONTENDO A MATRIZ CONFUSAO.
         def set_dados(self,conf_mat):
                 self.dados = conf_mat
+        ##################################################################################################################################################################################################
         def set_escore_erro(self,escErr):
                 self.escore_erro = escErr
         def set_escore_acerto(self, escAce):
                 self.escore_acerto = escAce
-        
+        def __str__(self):
+                string = ""
+                for i in range(self.nclasses):
+                        string+="\nClasse "+str(i)+" :"
+                        string+="Acc = {:014.10f}%\t".format((self.dados[i,i]/self.nTeste)*100.0)
+                        string+="Acc++ = {:014.10f}%\t".format(((sum(self.dados[i,:i+2] if (i==0) else self.dados[i,i-1:i+2]))/self.nTeste)*100.0)
+                        string+="Acertos = {:05.02f}\t".format(self.dados[i,i])
+                        string+="Acertos++ = {:05.02f}\t".format(sum(self.dados[i,:i+2] if (i==0) else self.dados[i,i-1:i+2]))
+                        string+="Erros = {:05.02f}\t".format(self.nTeste - self.dados[i,i])
+                        string+="Erros++ = {:05.02f}".format(self.nTeste - sum(self.dados[i,:i+2] if (i==0) else self.dados[i,i-1:i+2]))
+                string+="\n\nMatriz Confusao\n"
+                for i in range(self.nclasses):
+                        string+="\nClasse {:02d}:\t".format(i)
+                        for j in range(self.nclasses):
+                                string+="{:05.02f}\t".format(self.dados[i,j])
+                        
+                return string
+##################################################################################################################################################################################################
+
 class rodada(object):
-        def __init__(self, nIteracoes, nclasses):
-                self.iteracoes = [iteracao(nclasses) for i in range(nIteracoes)]
-                self.avg_err = np.zeros((nclasses,1))
-                self.avg_ace = np.zeros((nclasses,1))
-                self.avg_cfm = np.zeros((nclasses,nclasses))
+        def __init__(self, nIteracoes, nclasses, nTreino = 13):
+                self.iteracoes = [iteracao(nclasses,nTreino) for i in range(nIteracoes)]
+                self.sum_err = np.zeros((nclasses,1))
+                self.sum_ace = np.zeros((nclasses,1))
+                self.sum_cfm = np.zeros((nclasses,nclasses))
                 self.num_ite = nIteracoes
                 self.num_cls = nclasses
-                
-        def avg_calculate(self):
-                self.avg_cfm = np.divide(self.avg_cfm, np.full((self.num_cls, self.num_cls), float(self.num_ite)))
-                self.avg_ace =  map(lambda x: x / self.num_ite ,self.avg_ace)
-                self.avg_err =  map(lambda x: x / self.num_ite , self.avg_err)
-            
         def set_iteracao(self,nIter,oIter):
                 self.iteracoes[nIter-1] = copy.copy(oIter)
-                self.avg_err = np.add(self.avg_err,self.iteracoes[nIter-1].escore_erro)
-                self.avg_ace = np.add(self.avg_ace,self.iteracoes[nIter-1].escore_acerto)
-                print  self.avg_err
-                #self.avg_calculate()
+                self.sum_err = np.add(self.sum_err,self.iteracoes[nIter-1].escore_erro)
+                self.sum_ace = np.add(self.sum_ace,self.iteracoes[nIter-1].escore_acerto)
+                self.sum_cfm = np.add(self.sum_cfm,self.iteracoes[nIter-1].dados)
+                
+        def get_avg_cfm(self):
+                return np.divide(self.sum_cfm,float(self.num_ite))
         
-                            
-                                
-                        
-a = np.full((7,7),10.0)
-b = np.full((7,7),20.0)
-c = [iteracao(7),iteracao(7)]
-c[0].set_dados(a)
-c[0].set_escore_erro(np.full((7,1),8.0))
-c[1].set_escore_erro(np.full((7,1),2.0))
-c[1].set_dados(b)
-d = rodada(3,7)
-d.set_iteracao(1,c[0])
-d.set_iteracao(2,c[1])
+        def get_avg_ace(self):
+                return np.matrix(map(lambda x:x/self.num_ite , self.sum_ace))
+        
+        def get_avg_err(self):
+                return np.matrix(map(lambda x:x/self.num_ite , self.sum_err))
+        def get_avg_acc(self):
+                soma = np.zeros((8,1))
+                soma_= np.zeros((8,1))
+                for i in self.iteracoes:
+                        for j in range(self.num_cls):
+                                soma[j] += (i.dados[j,j]/i.nTeste)
+                                soma_[j] += ((sum(i.dados[j,:j+2] if (j==0) else i.dados[j,j-1:j+2]))/i.nTeste)
+                soma = np.multiply(soma,1.0/float(self.num_ite))
+                soma_ = np.multiply(soma_,1.0/float(self.num_ite))
+                soma[self.num_cls] = sum(soma[:self.num_cls])/float(self.num_cls)
+                soma_[self.num_cls] = sum(soma_[:self.num_cls])/float(self.num_cls)
+                return soma,soma_
+        def save(self,path):
+                pk.dump(self, open(path,"wb"))
+                print "Arquivo salvo com sucesso em ",path
+        
+        def load(self,path):
+                return copy.copy(pk.load(open(path,"rb")))
+        
+        def __str__(self):
+                string = ""
+                for i in range(self.num_ite):
+                        string+="\n\n\n--------------------------------- iteração {:03d} -----------------------------------".format(i+1)
+                        string+="\n\t---------------------------- KERNEL {} -----------------------------".format(self.iteracoes[i].svm_params['kernel_type'])
+                        string+="\n\t-------------------------- GAMMA {:05.02f} ----------------------------".format(self.iteracoes[i].svm_params['gamma'])
+                        string+=str(self.iteracoes[i])
+                        string+="\n----------------------------------------------------------------------------------------\n"
+                for i in range(self.num_cls+1):
+                        string+= "\nClasse {:02d}:\t".format(i)
+                        string+= "Acc = {:014.10f}%\t".format(self.get_avg_acc()[0][i,0]*100)
+                        string+= "Acc++ = {:014.10f}%\t".format(self.get_avg_acc()[1][i,0]*100)
+                return string
 
-
+if __name__== "__main__":
+        obj = rodada(50,7)
+        obj = obj.load("OBJETOS/PASSO_DECIMACAO-099%-050Iteracoes-PESOS_TIPO_01.pkl")
+        print obj
+        
---
+++ Python/svm_gera_objetos.py
@@ -0,0 +1,93 @@
+from Classes import *
+oAddress = "OBJETOS/"
+vAddress = "VETORES/"
+Metodo = "PASSO_DECIMACAO"
+PesosTipo = "01"
+iteracoes = 50
+qtdtreino = 37
+qtdTeste = 49-qtdtreino
+#Pesos de erro para calculo do Score_error
+pesos = [[  0.   ,1.   ,2.   ,3.   ,4.   ,5.   ,6.],
+         [  1.   ,0.   ,1.   ,2.   ,3.   ,4.   ,5.],
+         [  2.   ,1.   ,0.   ,1.   ,2.   ,3.   ,4.],
+         [  3.   ,2.   ,1.   ,0.   ,1.   ,2.   ,3.],
+         [  4.   ,3.   ,2.   ,1.   ,0.   ,1.   ,2.],
+         [  5.   ,4.   ,3.   ,2.   ,1.   ,0.   ,1.],
+         [  6.   ,5.   ,4.   ,3.   ,2.   ,1.   ,0.]]
+pesos = np.matrix(pesos);
+########################################################################################################################################################
+
+#Pesos de classe para calculo do Score_correct
+pesosCorr = [7.   ,6.   ,5.   ,4.   ,3.   ,2.   ,1.]
+########################################################################################################################################################
+
+for percent in range(75,100):
+    obj = rodada(iteracoes,7)
+    bd = ler_arquivo("GLCM_RESIZE/"+Metodo+"/GLCM_"+str(percent*100)+".txt")
+    bd = Normalizar(bd,len(bd),len(bd[0])-1)    
+    for it in range(iteracoes):
+        oIt = iteracao(7,13)
+        atributos = []
+        labels = []
+        TreinoLabel = []
+        Treino = []
+        for i in bd:
+            atributos.append(i[:9])
+            labels.append([i[-1]-1])
+        ##Atributo da classe 0
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,0,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 1
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,1,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 2
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,2,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 3
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,3,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 4
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,4,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 5
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,5,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+        
+        ##Atributo da classe 6
+        atributos,labels,Treino,TreinoLabel =  Train_extract(atributos,labels,6,qtdtreino,qtdTeste,Treino,TreinoLabel)
+        ########################################################################################################################################################
+
+        oIt.conj_treino = np.float32(np.matrix(Treino))
+        oIt.conj_teste = np.matrix(atributos)
+        oIt.conj_treino_label = np.float32(np.matrix(TreinoLabel))
+        oIt.conj_teste_label = np.matrix(labels)
+        oIt.svm_params = dict(kernel_type = cv2.SVM_RBF,
+                svm_type = cv2.SVM_C_SVC,
+                C=7.0,
+                degree =1.0,
+                gamma=2,
+                nu = 0.0,
+                p = 0.0,
+                coef0 = 0,
+                class_weights = None,
+                epsilon = 1e-6
+                )
+        svm = cv2.SVM()
+        svm.train(np.float32(Treino),np.float32(TreinoLabel),params = oIt.svm_params)
+        for tsample in range(len(atributos)):
+            test_sample = np.float32(atributos[tsample])
+            res = int(svm.predict(test_sample))
+            test = int(labels[tsample][0])
+            oIt.dados[test,res] +=1
+        mul = np.multiply(oIt.dados,pesos)
+        oIt.escore_erro = np.matrix(map(lambda x: np.sum(x) ,mul))
+        oIt.escore_acerto = np.matrix([pesosCorr[i]*oIt.dados[i,i] for i in range(7)])
+        obj.set_iteracao(it+1,oIt)
+    obj.save(oAddress+"{}-{:03d}%-{:03d}Iteracoes-PESOS_TIPO_{}.pkl".format(Metodo,percent,iteracoes,PesosTipo))
+    
+        
