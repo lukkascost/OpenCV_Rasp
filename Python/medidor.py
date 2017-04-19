158310319fee57d968faf18663f380a4816c55f4
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2017-04-19 02:55:33 +0000

    Gerando tempos de passo 1 em 1

--- Python/medidor.py
+++ Python/medidor.py
@@ -1,38 +1,11 @@
 import subprocess
 from Metodos import *
-de = 90
+de = 1
 ate = 100
-passo = 5
+passo = 1
 bateria = 1
 qtdBaterias = 1
-local = "PC"
-result = ""
-importres = []
-##
-print "Medindo RESIZE + GLCM ..... "
-for j,i in enumerate(range(de,ate+1,passo)):
-	print "\tPasso: ",i
-	percent = 3456.0/float(i)
-	percent /= 3456.0
-	percent *= 100.0
-	result +=  "---------  {:04.03f}% da imagem passo {:03d}\n".format(percent,i)
-	command = " python -m timeit -n {:d} -r {:d}".format(bateria, qtdBaterias)
-	command += ' "from Metodos import *"'
-	command += ' "GLCM(resize_img_passo(cv2.imread('
-	command += "'DataBase/c1_1.JPG',0),"+str(i)+"),1.0)"
-	command += '"'
-	result +=  command +"\n"
-	importres.append(subprocess.check_output(command, shell=True)+" "+str(i))
-	result += "\t\t"+importres[j]
-	result += "\n"
-Salvar_texto(result,"../RESULTADOS/tempos_{}_RESIZE+GLCM.txt".format(local))
-texto = ""
-for i in importres:
-	texto+= str(i.split(" ")[5])+";"
-	texto+= str(i.split(" ")[6])+";"
-	texto+= str(i.split(" ")[9])+";"
-	texto+= "\n"
-Salvar_texto(texto,"../RESULTADOS/import_tempos_{}_RESIZE+GLCM.txt".format(local))
+local = "RaspBerry"
 importres = []
 result = ""
 ##
@@ -60,6 +33,33 @@ for i in importres:
 	texto+= str(i.split(" ")[9])+";"
 	texto+= "\n"
 Salvar_texto(texto,"../RESULTADOS/import_tempos_{}_RESIZE.txt".format(local))
+result = ""
+importres = []
+##
+print "Medindo RESIZE + GLCM ..... "
+for j,i in enumerate(range(de,ate+1,passo)):
+	print "\tPasso: ",i
+	percent = 3456.0/float(i)
+	percent /= 3456.0
+	percent *= 100.0
+	result +=  "---------  {:04.03f}% da imagem passo {:03d}\n".format(percent,i)
+	command = " python -m timeit -n {:d} -r {:d}".format(bateria, qtdBaterias)
+	command += ' "from Metodos import *"'
+	command += ' "GLCM(resize_img_passo(cv2.imread('
+	command += "'DataBase/c1_1.JPG',0),"+str(i)+"),1.0)"
+	command += '"'
+	result +=  command +"\n"
+	importres.append(subprocess.check_output(command, shell=True)+" "+str(i))
+	result += "\t\t"+importres[j]
+	result += "\n"
+Salvar_texto(result,"../RESULTADOS/tempos_{}_RESIZE+GLCM.txt".format(local))
+texto = ""
+for i in importres:
+	texto+= str(i.split(" ")[5])+";"
+	texto+= str(i.split(" ")[6])+";"
+	texto+= str(i.split(" ")[9])+";"
+	texto+= "\n"
+Salvar_texto(texto,"../RESULTADOS/import_tempos_{}_RESIZE+GLCM.txt".format(local))
 importres = []
 result = ""
 ##
