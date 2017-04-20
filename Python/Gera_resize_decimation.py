7877a8c03013d38b4c57759e36d1ee97bcbf1341
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-04-19 22:25:42 -0300

    43 a 4

--- Python/Gera_resize_decimation.py
+++ Python/Gera_resize_decimation.py
@@ -1,7 +1,7 @@
 import thread
 from Metodos import *
 percent = 90
-for i in range(99,1,-1):
+for i in range(43,1,-1):
         cria_Arquivo_GLCM(i,6,"PASSO_ROI_PRETO", '../../couro_images_semfundo',roi=True)
           
 
