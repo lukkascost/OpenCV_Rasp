1b8ed2fedf0999a33e3dbaec9bbddffb32292a41
lucas <lucas.costa@lit.ifce.edu.br>
2017-04-21 21:18:13 -0300

    mesmo de antes

--- Python/Gera_resize_decimation.py
+++ Python/Gera_resize_decimation.py
@@ -3,7 +3,9 @@ from Metodos import *
 percent = 90
 #for i in range(43,1,-1):
 #        cria_Arquivo_GLCM(1,6,"PASSO_ROI_PRETO", '../../couro_images_semfundo',roi=True)
+bd = []
 for i in range(1,8):
     for j in range(1,51):
-        GLCM(cv2.imread("../../couro_images_semfundo/c{:d}_{:d}.JPG".format(i,j),0))
+        bd.append(GLCM(cv2.imread("../../couro_images_semfundo/c{:d}_{:d}.JPG".format(i,j),0)))
         print i,j
+Salvar_arquivo(bd,"GLCM_RESIZE/PASSO_ROI_PRETO/GLCM_100.txt")
