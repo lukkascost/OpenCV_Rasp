2bfcdb3ee272e5c101d9470705734878c11f26cf
lucas <lucas.costa@lit.ifce.edu.br>
2017-04-21 21:15:16 -0300

    gerar arquivo GLCM com imagem sem resize

--- Python/Gera_resize_decimation.py
+++ Python/Gera_resize_decimation.py
@@ -1,7 +1,9 @@
 import thread
 from Metodos import *
 percent = 90
-for i in range(43,1,-1):
-        cria_Arquivo_GLCM(i,6,"PASSO_ROI_PRETO", '../../couro_images_semfundo',roi=True)
-          
-
+#for i in range(43,1,-1):
+#        cria_Arquivo_GLCM(1,6,"PASSO_ROI_PRETO", '../../couro_images_semfundo',roi=True)
+for i in range(1,8):
+    for j in range(1,51):
+        GLCM(cv2.imread("../../couro_images_semfundo/c{:d}_{:d}.JPG".format(i,j),0))
+        print i,j
