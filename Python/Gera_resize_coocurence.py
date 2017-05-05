9f210bddfc4d00de44ac0acd6293bba10df04928
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-05-05 15:13:24 -0300

    GLCM 128 e 64

--- Python/Gera_resize_coocurence.py
+++ Python/Gera_resize_coocurence.py
@@ -1,19 +1,19 @@
 import thread
 from Metodos import *
 bd = []
-for i in range(1,8):
-    for j in range(1,50):
-        k = 2
-        gray = 256
-        img = cv2.imread("../../couro_images_semfundo/c{:d}_{:d}.JPG".format(i,j),0)
-        for x in range(k):
-            img = np.divide(img,2)
-            gray = gray/2
-        coOccurence = getCoOccurrenceMatrixMod(img, gray)
-        coOccurenceNormalized = normalizeCoOccurrenceMatrix(coOccurence,img,gray)
-        glcm_features  = getFeatures(coOccurenceNormalized, gray)
-        glcm_features[9] = i
-        bd.append(glcm_features)
-        print "Imagem {:02d} classe {:02d} {:02d} Divisoes coOccurrence tamanho {:03d}".format(j,i, k,gray)
-        print glcm_features
-Salvar_arquivo(bd,"GLCM_RESIZE/GLCM_{:03d}GScale.txt".format(gray))
+for k in range(1,8):
+    for i in range(1,8):
+        for j in range(1,51):
+            gray = 256
+            img = cv2.imread("../../couro_images_semfundo/c{:d}_{:d}.JPG".format(i,j),0)
+            for x in range(k):
+                img = np.divide(img,2)
+                gray = gray/2
+            coOccurence = getCoOccurrenceMatrixMod(img, gray)
+            coOccurenceNormalized = normalizeCoOccurrenceMatrix(coOccurence,img,gray)
+            glcm_features  = getFeatures(coOccurenceNormalized, gray)
+            glcm_features[9] = i
+            bd.append(glcm_features)
+            print "Imagem {:02d} classe {:02d} {:02d} Divisoes coOccurrence tamanho {:03d}".format(j,i, k,gray)
+            print glcm_features
+    Salvar_arquivo(bd,"GLCM_RESIZE/GLCM_{:03d}GScale.txt".format(gray))
