99eeff12ebdd18d3510f5901fa67697fac99095c
Lucas  Silva <lucassilva@MacBook-Air-de-Lucas.local>
2017-05-22 19:01:28 -0300

    tabela de medias e variancias.

--- Python/comb_res.py
+++ Python/comb_res.py
@@ -10,24 +10,25 @@ pesos = [[  0.   ,15.  ,25.   ,45.   ,60.   ,80.   ,85.],
          [  0.   ,0.   ,0.    ,0.    ,0.    ,0.    ,0.]]
 pesosCorr = [100.   ,85.   ,75.   ,55.   ,40.   ,20.   ,15.]
 combs = []
-caracteres = [x for x in range(1,15)]
+caracteres = [x for x in range(1,25)]
 
-positions.append(list(combinations(caracteres, 1)))
+positions.append(list(combinations(caracteres, 4)))
 for i in positions[0]:
     combs.append(list(i))
-passos = [1]
-metodo = "PASSO_ROI_PRETO"
-it = 50
-coisas = []
-obj = rodada(it,7,nAtrib=14)
-for k,positions in enumerate(combs):
-    for percent in passos:
-        obj = obj.load("OBJETOS/COMBINACOES/{:02d}-{}-{:03d}%-{:03d}Iteracoes_7C3T.pkl".format(02,positions,percent,it))
-        acc = float(obj.get_avg_acc()[0][7])
-        std = float(obj.get_std_acc()[7])
-        coisas.append([acc-std,acc+std,positions])
-        coisas.sort(key=lambda tup: tup[0], reverse=1)
-        for h,i in enumerate(coisas):
-            print i
-            #if h == 9: break
-        print "\n\n\n"
+print len(combs)
+#passos = [1]
+#metodo = "PASSO_ROI_PRETO"
+#it = 50
+#coisas = []
+#obj = rodada(it,7,nAtrib=14)
+#for k,positions in enumerate(combs):
+    #for percent in passos:
+        #obj = obj.load("OBJETOS/COMBINACOES/{:02d}-{}-{:03d}%-{:03d}Iteracoes_7C3T.pkl".format(02,positions,percent,it))
+        #acc = float(obj.get_avg_acc()[0][7])
+        #std = float(obj.get_std_acc()[7])
+        #coisas.append([acc-std,acc+std,positions])
+        #coisas.sort(key=lambda tup: tup[0], reverse=1)
+        #for h,i in enumerate(coisas):
+            #print i
+            ##if h == 9: break
+        #print "\n\n\n"
