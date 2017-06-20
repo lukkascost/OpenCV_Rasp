2ede21226dec483c34fcb0639e04849c549bc8db
Lucas  Silva <lucassilva@air-de-lucas.localdomain>
2017-06-20 10:45:08 -0300

    criado um metodo para gerar os 24 atributos implementados na mao por mim

--- Python/teste.py
+++ Python/teste.py
@@ -1,29 +1,12 @@
-from Metodos import *
-import svmlight as sl
-positions = [1,3,5,14]
-obj = rodada(50, 7, nAtrib=14).load("OBJETOS/COMBINACOES/02-[1, 3, 5, 14]-001%-050Iteracoes_7C3T.pkl")
-train = []
-trainLabel =[]
-for k in obj.iteracoes[0].conj_treino:
-    train.append(obj.GLCM.getNewAtrib(k,positions))
-    trainLabel.append(obj.GLCM.labels[k])
-    
-Training_data = []
-for i,j in enumerate(train):
-    newj = []
-    for o,k in enumerate(j):
-        newj.append((o,k))
-    Training_data.append(tuple([trainLabel[i]]+[newj]))
-Training_data = tuple(Training_data)  
-model = sl.learn(Training_data, kernel="rbf",rbf_gamma=2.0,verbosite=0)
-sl.write_model(model, "teste.svml")
-Testing_data = []
-for i,j in enumerate(obj.iteracoes[0].conj_teste):
-    newj = []
-    for o,k in enumerate(obj.GLCM.atributos[j]):
-        if o+1 in positions:
-            newj.append((positions.index(o+1),k))
-    Testing_data.append([obj.GLCM.labels[j]]+[newj])
-Training_data = tuple(Training_data)
-predictions = sl.classify(model, tuple(Testing_data))
-print predictions
+from Classes import *
+positions = [x for x in range(1,25)]
+obj = rodada(50,7,nAtrib=24)
+obj = obj.load("OBJETOS/{:02d}-{}-{:03d}%-{:03d}Iteracoes_7C3T.pkl".format(02,positions,1,50))
+obj.execIteractions([1,3,5,14])
+print obj.get_avg_acc()[0][7] , obj.get_std_acc()[7] , [1,3,5,14]
+obj.execIteractions(positions)
+print obj.get_avg_acc()[0][7] , obj.get_std_acc()[7] , positions
+obj.execIteractions([1,3,10,14,17,20,22])
+print obj.get_avg_acc()[0][7] , obj.get_std_acc()[7] , [1,3,10,14,17,20,22]
+obj.execIteractions([1,2,5,9,15,16,17,21,22,23])
+print obj.get_avg_acc()[0][7] , obj.get_std_acc()[7] , [1,2,5,9,15,17,16,21,22,23]
