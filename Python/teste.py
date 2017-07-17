8ec0162e0821ad5550e137c4aa56ddb0a9d3485f
LucasCosta <lucas.costa@wdatecnologia.com.br>
2017-07-16 22:49:39 -0300

    graficos

--- Python/teste.py
+++ Python/teste.py
@@ -1,4 +1,47 @@
+from Metodos import *
+import subprocess
 
+passos = []
+lista = []
+for i in range(1,1000):
+        if (3456/i,4608/i) not in lista:
+                lista.append((3456/i,4608/i))
+                passos.append(i)
+passos = [11,33,34,68,69,105,108,165,171,922]
+passos.sort(reverse=0)
+valors = np.zeros((10,10))
+stri = ""
+for i,j in enumerate(passos):
+        arquivo = open("../RESULTADOS/time_GLCM_{}_lucas10.txt".format(j),"r")
+        linha = arquivo.readline()[1:]
+        linha = linha[:-1].split(",")
+        for l,k in enumerate(linha):
+                valors[i,l] = float(k)
+        stri+= str((3456/j,4608/j))+"\t"
+        print (3456/j,4608/j), j
+print stri
+medias = []
+stri = ""
+for i in valors:
+        stri+= "{:03.04f}\t".format(np.average(i))
+print stri
+
+valors = np.zeros((10,10))
+stri = ""
+for i,j in enumerate(passos):
+        arquivo = open("../RESULTADOS/time_GLCM_{}_CFS.txt".format(j),"r")
+        linha = arquivo.readline()[1:]
+        linha = linha[:-1].split(",")
+        for l,k in enumerate(linha):
+                valors[i,l] = float(k)
+        stri+= str((3456/j,4608/j))+"\t"
+        print (3456/j,4608/j), j
+print stri
+medias = []
+stri = ""
+for i in valors:
+        stri+= "{:03.04f}\t".format(np.average(i))
+print stri
 
 
 #from Classes import *
