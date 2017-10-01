05cc179ac6c6e69ada59c0875d2738a87cab74dd
LucasCosta <lucas.costa@wdatecnologia.com.br>
2017-10-01 17:14:13 -0300

    resultados do LBP

--- Python/LBP_SVM.py
+++ Python/LBP_SVM.py
@@ -21,34 +21,34 @@ def LBP_img(img, att58):
         return histogran
 passos = []
 lista = []
-for i in range(1,23):
+for i in range(1,1000):
         if (3456/i,4608/i) not in lista:
                 lista.append((3456/i,4608/i))
                 passos.append(i)
 passos.sort(reverse=0)
 
 #######################################################################################################################################
-for k in passos:        
-        print k
-        bd = []
-        for i in range(1,8):
-                print i      
-                for j in range(1,51):
-			print i,j
-                        img = cv2.imread("../../couro_images_semfundo/c{}_{}.JPG".format(i,j),0)
-                        img = resize_img_passo(img,k)
-                        lbp = LBP_img(img, att58)
-                        lbp[-1] = i
-                        bd.append(lbp)
-                Salvar_arquivo(bd,"LBP/FEATURES_M{}.txt".format(k))
+#for k in passos:        
+        #print k
+        #bd = []
+        #for i in range(1,8):
+                #print i      
+                #for j in range(1,51):
+			#print i,j
+                        #img = cv2.imread("../../couro_images_semfundo/c{}_{}.JPG".format(i,j),0)
+                        #img = resize_img_passo(img,k)
+                        #lbp = LBP_img(img, att58)
+                        #lbp[-1] = i
+                        #bd.append(lbp)
+                #Salvar_arquivo(bd,"LBP/FEATURES_M{}.txt".format(k))
 #######################################################################################################################################
 nclasses = 7        
-positions = [x for x in range(256)]
+positions = [x for x in range(257)]
 for metodo in ["PASSO_ROI_PRETO"]:
         it = 50
         for percent in passos:
                 print percent
-                obj = rodada(it,nclasses,nAtrib=256,nTreino=13, nAmostras=50)
+                obj = rodada(it,nclasses,nAtrib=257,nTreino=13, nAmostras=50)
                 bd = ler_arquivo("LBP/FEATURES_M{:d}.txt".format(percent))
                 bd = Normalizar(bd,len(bd),len(bd[0])-1)
                 for i in bd:
@@ -85,11 +85,13 @@ lin4 = ""
 for percent in passos:
         obj = rodada(1, 1)
         obj = obj.load("OBJETOS/LBP_{:02d}-{}-{:03d}%-{:03d}Iteracoes_7C3T.pkl".format(2,255,percent,50))
+	p2 = [x for x in range(1,257)]
+	obj.execIteractions(p2)
         lin1+=  "{:02.04f}\t".format(obj.get_avg_acc()[0][-1][0])
-        p2 = [1, 2, 3, 4, 5, 7, 8, 9, 13, 15, 16, 17, 25, 29, 31, 32, 33, 49, 57, 61, 63, 64, 65, 97, 113, 121, 125, 127, 128, 129, 130, 132, 136, 144, 160, 192, 193, 194, 196, 200, 208, 224, 225, 226, 228, 232, 240, 241, 242, 244, 248, 249, 250, 252, 253, 254, 255,256]
+        p2 = [1, 2, 3, 4, 5, 7, 8, 9, 13, 15, 16, 17, 25, 29, 31, 32, 33, 49, 57, 61, 63, 64, 65, 97, 113, 121, 125, 127, 128, 129, 130, 132, 136, 144, 160, 192, 193, 194, 196, 200, 208, 224, 225, 226, 228, 232, 240, 241, 242, 244, 248, 249, 250, 252, 253, 254, 255, 256, 257]
         obj.execIteractions(p2)
         lin2+=  "{:02.04f}\t".format(obj.get_avg_acc()[0][-1][0])
-        p2 = [2, 33, 128, 193, 244, 248, 249, 250, 254, 256]
+        p2 = [2, 33, 128, 193, 244, 248, 249, 250, 254, 256, 257]
         obj.execIteractions(p2)
         lin3+=  "{:02.04f}\t".format(obj.get_avg_acc()[0][-1][0])
         #p2 = [1,2,5,9,15,16,17,21,22,23]
