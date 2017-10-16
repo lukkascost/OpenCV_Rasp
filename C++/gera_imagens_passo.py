ca44fd9ff0927f97e7e9ac30fa5363e1be1e2cd6
Lukkascost <lucas.costa@wdatecnologia.com.br>
2017-10-16 17:30:18 -0300

    images leandro decimation for time and memory measurements

--- C++/gera_imagens_passo.py
+++ C++/gera_imagens_passo.py
@@ -2,13 +2,13 @@ from Metodos import *
 
 lista = []
 passos = []
-for i in range(100,1000):
-    if (3456/i,4608/i) not in lista:
-        lista.append((3456/i,4608/i))
+for i in range(2,1000):
+    if (4000/i,3000/i) not in lista:
+        lista.append((4000/i,3000/i))
         passos.append(i)
 passos.sort(reverse=True)
 
 for i in passos:
-        img = cv2.imread("../../couro_images/c1_1.JPG",0)
+        img = cv2.imread("Imagens_Leandro/001_passo.JPG",0)
         img = resize_img_passo(img,i)
-        cv2.imwrite("Imagens/{:03d}_passo.JPG".format(i),img)
+        cv2.imwrite("Imagens_Leandro/{:03d}_passo.JPG".format(i),img)
