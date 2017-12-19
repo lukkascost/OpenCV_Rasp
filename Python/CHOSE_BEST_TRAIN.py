4aa85d1bc8161bf832aa28a480ffcd181b91fc22
lukkascost <lucas.costa@lit.ifce.edu.br>
2017-12-19 12:24:41 -0300

    Clean code

--- Python/CHOSE_BEST_TRAIN.py
+++ Python/CHOSE_BEST_TRAIN.py
@@ -17,24 +17,24 @@ nVetores = 10000;
 
 #for i in range(1):
         #oDataSet = DataSet()
-        #base = np.loadtxt("GLCM_RESIZE/DECIMACAO_BASE_24/FEATURES_M70.txt",delimiter=",")
+        #base = np.loadtxt("GLCM_RESIZE/results-norm.dat",delimiter=", ")
         #for k in base:
                 #oDataSet.addSampleOfAtt(k[basemask])
         #oDataSet.normalizeDataSet()
         #for j in range(10000):
                 #oData  = Data(7, 13)
-                #oData.radndomTrainingTestPerClass()
+                #oData.randomTrainingTestPerClass()
                 #svm = cv2.SVM()
                 #oData.params = dict(kernel_type = cv2.SVM_RBF,svm_type = cv2.SVM_C_SVC,gamma=2.0,nu = 0.0,p = 0.0, coef0 = 0)
                 #svm.train(np.float32( oDataSet.atributes[oData.Training_indexes]) , np.float32( oDataSet.labels[oData.Training_indexes]) , params = oData.params)
                 #results = svm.predict_all(np.float32(oDataSet.atributes[oData.Testing_indexes]))    
                 #oData.setResultsFromClassfier(results, oDataSet.labels[oData.Testing_indexes])
-                #if oData.getAccuraceAllClass() > acuracia:
+                #if oData.getMetrics()[0,-1] > acuracia:
                         #nVetores= svm.get_support_vector_count()
-                        #acuracia= oData.getAccuraceAllClass()
+                        #acuracia= oData.getMetrics()[0,-1] 
                         #print nVetores, acuracia
                         #best = j
-                #elif (oData.getAccuraceAllClass() == acuracia):
+                #elif (oData.getMetrics()[0,-1] == acuracia):
                         #if nVetores > svm.get_support_vector_count():
                                 #nVetores= svm.get_support_vector_count()
                                 #best = j
@@ -48,11 +48,10 @@ nVetores = 10000;
 ################################################################################################################################
 oExp = oExp.load("OBJETOS/EXPERIMENTO_01_MELHOR_TREINAMENTO_10000_COURO.txt")
 print oExp
-oData = oExp.experimentResults[0].dataSet[9170]
+oData = oExp.experimentResults[0].dataSet[4346]
 oDataSet = oExp.experimentResults[0]
 
-print "Acuracias por classe: \n", oData.getAccuracePerClass()
-print "Acuracia Geral: \n",oData.getAccuraceAllClass()
+print "Acuracias por classe: \n", oData.getMetrics().T
 print "Matriz Confusao: \n",oData.confusion_matrix
 
 svm = cv2.SVM()
