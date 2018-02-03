d35083323fe21f6a52edea26cc7b839e4378e74a
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-02-03 12:46:32 -0300

    graphcs

--- Python/METER_TIME_CLOCK_ROBOT.py
+++ Python/METER_TIME_CLOCK_ROBOT.py
@@ -83,17 +83,35 @@ def measureTime(img,bits=6):
         
         
 
-## medicoes para m=1 e variando a matriz de correlacao.
-imgs_times = np.zeros((8,5))
-for b in range(1,9):
-        total_times = []
-        for i in range(100):
-                total_times.append(measureTime(cv2.imread(path1+"050_passo.JPG",0),bits=b))
-                print total_times[-1],i
-        total_times = np.array(total_times)
-        imgs_times[b-1,:4] = np.min(total_times,axis=0)
-        imgs_times[b-1,-1] = np.sum(np.min(total_times,axis=0))
-        print imgs_times  
-        np.savetxt("../RESULTADOS/T1_M50_CM1-8b.txt", imgs_times, delimiter=',')
-        np.savetxt("../RESULTADOS/T1_M50_CM1-8b_Line_{:03d}.txt".format(b-1), total_times, delimiter=',')
-        
+### medicoes para m=1 e variando a matriz de correlacao.
+#imgs_times = np.zeros((8,5))
+#for b in range(1,9):
+        #total_times = []
+        #for i in range(100):
+                #total_times.append(measureTime(cv2.imread(path1+"050_passo.JPG",0),bits=b))
+                #print total_times[-1],i
+        #total_times = np.array(total_times)
+        #imgs_times[b-1,:4] = np.min(total_times,axis=0)
+        #imgs_times[b-1,-1] = np.sum(np.min(total_times,axis=0))
+        #print imgs_times  
+        #np.savetxt("../RESULTADOS/T1_M50_CM1-8b.txt", imgs_times, delimiter=',')
+        #np.savetxt("../RESULTADOS/T1_M50_CM1-8b_Line_{:03d}.txt".format(b-1), total_times, delimiter=',')
+
+
+### T2_M100,50,10,1_CM5b
+strResult = ""
+for k in range(4):
+        for i,j in enumerate([100,50,10,1]):
+                totals = np.loadtxt("../RESULTADOS/T2_M100,50,10,1_CM5b_Line_{:03d}.txt".format(i),delimiter=",")*1000
+                strResult+= str(round(np.average(totals,axis=0)[k],4)) + " +- "   
+                strResult+= str(round(np.std(totals,axis=0)[k],4)) + "\t" 
+        strResult += "\n"
+
+for i,j in enumerate([100,50,10,1]):
+        totals = np.loadtxt("../RESULTADOS/T2_M100,50,10,1_CM5b_Line_{:03d}.txt".format(i),delimiter=",")*1000
+        strResult += str(np.round(np.sum(np.average(totals,axis=0)),4)) + " +- "   
+        strResult += str(np.round(np.sum(np.std(totals,axis=0)),4)) + "\t" 
+print strResult
+
+
+print "-"*30
--- RESULTADOS/README.md
+++ RESULTADOS/README.md
@@ -12,7 +12,7 @@
         
 ## T2_M100,50,10,1_CM5b.txt
 ### Terminação _Line_***.txt indica valores que formam a linha do arquivo principal.
-        M variando em 4 valores \(1,10,50,100) , CM de 5bits, banco de imagem ROBOT
+        M variando em 4 valores (1,10,50,100) , CM de 5bits, banco de imagem ROBOT
         Contem um numpy array separado por virgula.
         Cada linha corresponde a um valor de M em ordem decrescente.(4 linhas) 100,50,10,1
         a primeira coluna contem o tempo para gerar a matriz de GLCM horizontal com vizinho direito.(s)
@@ -20,6 +20,19 @@
         a terceira coluna contem o tempo para gerar os 10 atributos mais rapidos.
         a quarta coluna contem o tempo para predizer uma amostra.
         a quinta coluna contem um somatorio das quatro anteriores formando o tempo total.
+
+## T1_M50_CM1-8b.txt
+### Terminação _Line_***.txt indica valores que formam a linha do arquivo principal.
+        M fixo em 50, CM variando de 1 a 8 bits, banco de imagem ROBOT
+        Contem um numpy array separado por virgula.
+        Cada linha corresponde a um valor de CM em ordem crescente.(8 linhas). 1b,2b,3b,..,8b.
+        a primeira coluna contem o tempo para gerar a matriz de GLCM horizontal com vizinho direito.(s)
+        a segunda coluna contem o tempo para normalizar a matriz de GLCM.
+        a terceira coluna contem o tempo para gerar os 10 atributos mais rapidos.
+        a quarta coluna contem o tempo para predizer uma amostra.
+        a quinta coluna contem um somatorio das quatro anteriores formando o tempo total.     
+        
+        
 
 * **Lucas Costa** - [lukkascost](https://github.com/lukkascost)
 
