5af6210bbcdeca1bbabd9642b7d0ff8a1a846797
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-02-02 22:52:11 -0300

    Results, and preparation for measure time

--- Python/METER_TIME_CLOCK_ROBOT.py
+++ Python/METER_TIME_CLOCK_ROBOT.py
@@ -3,14 +3,14 @@ import time as tm
 import cv2
 from MachineLearn.Classes.Extractors.GLCM import GLCM
 
+path1 = "../C++/Imagens_Leandro/"
 
-def measureTime(img):
+def measureTime(img,bits=6):
         
         """
         Input data
         """        
         ########################################################################
-        bits = 6
         img = img/2**(8-bits)
         oGlcm = GLCM(img, bits, number_of_Attributes=10)
         basemask = np.array([1,2,5,9,15,16,17,21,22,23])
@@ -67,27 +67,32 @@ def measureTime(img):
         times[3] = end-start
         ########################################################################
         return times
-path = "../RESULTADOS/TIMES_FOR_EACH_IMG.txt"
-#imgs_times = np.zeros((350,5))
-#for j in range(7):
-        #for k in range(50):
-                #total_times = []
-                #for i in range(100):
-                        #total_times.append(measureTime(cv2.imread("img_resize_73/c{}_{}.JPG".format(j+1,k+1),0)))
-                #total_times = np.array(total_times)
-                #imgs_times[k+(j*50),:4] = np.min(total_times,axis=0)
-                #imgs_times[k+(j*50),-1] = np.sum(np.min(total_times,axis=0))*1000 
-                ##print np.sum(np.min(total_times,axis=0))*1000          
-                #print imgs_times[k+(j*50)],k+(j*50)
-                #np.savetxt("../RESULTADOS/TIMES_FOR_EACH_IMG.txt", imgs_times, delimiter=',')
-                
-array = np.loadtxt(path,delimiter=",")
-print "Menor tempo total: ", np.min(array,axis=0)[-2]
-print "Maior tempo total: ", np.max(array,axis=0)[-2]
-array = array[:,-2]
-array =  array.reshape((7,50,1))
-array = array*1000000
 
+## medicoes para cm=5b e variando o M.
+imgs_times = np.zeros((4,5))
+for k,m in enumerate([101,50,10,1]):
+        total_times = []
+        for i in range(100):
+                total_times.append(measureTime(cv2.imread(path1+"{:03d}_passo.JPG".format(m),0),bits=5))        
+        total_times = np.array(total_times)
+        imgs_times[k,:4] = np.min(total_times,axis=0)
+        imgs_times[k,-1] = np.sum(np.min(total_times,axis=0))   
+        print imgs_times   
+        np.savetxt("../RESULTADOS/T2_M100,50,10,1_CM5b.txt", imgs_times, delimiter=',')
+        np.savetxt("../RESULTADOS/T2_M100,50,10,1_CM5b_Line_{:03d}.txt".format(k), total_times, delimiter=',')
+        
+        
 
-for i,a in enumerate(array):
-        print "Class: ",i+1,"\tAVG: ",np.average(a) ," \tSTD: " , np.std(a)
+## medicoes para m=1 e variando a matriz de correlaçao.
+imgs_times = np.zeros((8,5))
+for b in range(1,9):
+        total_times = []
+        for i in range(100):
+                total_times.append(measureTime(cv2.imread(path1+"001_passo.JPG",0),bits=b))
+        total_times = np.array(total_times)
+        imgs_times[b-1,:4] = np.min(total_times,axis=0)
+        imgs_times[b-1,-1] = np.sum(np.min(total_times,axis=0))
+        print np.sum(np.min(total_times,axis=0))*1000   
+        np.savetxt("../RESULTADOS/T1_M1_CM1-8b.txt", imgs_times, delimiter=',')
+        np.savetxt("../RESULTADOS/T1_M1_CM1-8b_Line_{:03d}.txt".format(b-1), total_times, delimiter=',')
+        
---
+++ RESULTADOS/README.md
@@ -0,0 +1,14 @@
+# PROJECT FOR CLASSIFY LEATHER IMAGES
+
+## Python
+
+## C++
+
+## RESULTS
+
+
+## Authors
+
+* **Lucas Costa** - [lukkascost](https://github.com/lukkascost)
+
+See also the list of [contributors](https://github.com/lukkascost/MachineLearn/contributors) who participated in this project.
