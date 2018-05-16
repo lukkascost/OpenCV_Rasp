ebc7adc16ee865983398a1b3ab9874927ec8c776
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-05-16 17:09:19 -0300

    glcm com limiarizacao 96 gerado.

--- Python/GLCM_RESIZE_GENERATE.py
+++ Python/GLCM_RESIZE_GENERATE.py
@@ -1,27 +1,35 @@
 from MachineLearn.Classes.Extractors.GLCM import GLCM
-import numpy as np 
+import numpy as np
 import cv2
-k = 50
-nbits = 5
+
+k = 73
+nbits = 8
 listGLCM = []
-for lin in range(15):
-        for col in range(40):
-                img = cv2.imread("../../Leandro/P{:02d}_{:02d}.JPG".format(lin+1,col+1),0)
-                print lin+1,col+1
-                ### decimacao 
-                klist = [x for x in range(0,img.shape[0],k)]
-                klist2 = [x for x in range(0,img.shape[1],k)]
-                img = img[klist]
-                img = img[:,klist2]
-                img = img/2**(8-nbits)                
-                oGlcm = GLCM(img, nbits)
-                oGlcm.generateCoOccurenceHorizontal()
-                oGlcm.normalizeCoOccurence()
-                oGlcm.calculateAttributes()                
-                listGLCM.append(oGlcm.exportToClassfier(lin+1))
-                
+for lin in range(7):
+    for col in range(50):
+        img = cv2.imread("../../Couro/c{:d}_{:d}.JPG".format(lin + 1, col + 1), 0)
+        print lin + 1, col + 1
+        ### decimacao
+        klist = [x for x in range(0, img.shape[0], k)]
+        klist2 = [x for x in range(0, img.shape[1], k)]
+        img = img[klist]
+        img = img[:, klist2]
+
+        ##Threshold
+        threshold = np.copy(img)
+        threshold[threshold < 96] = 0
+        threshold[threshold >= 96] = 1
+        img = img * threshold
+
+        # img = img / 2 ** (8 - nbits)
+        oGlcm = GLCM(img, nbits)
+        oGlcm.generateCoOccurenceHorizontal()
+        oGlcm.normalizeCoOccurence()
+        oGlcm.calculateAttributes()
+        listGLCM.append(oGlcm.exportToClassfier(lin + 1))
 listGLCM = np.array(listGLCM)
 print listGLCM
-                
-np.savetxt("GLCM_RESIZE/DECIMACAO_BASE_LEANDRO/FEATURES_M50_CM5b.txt", listGLCM, fmt='%.18e', delimiter=',', newline='\n', header='', 
-          footer='', comments='# ')
+
+np.savetxt("GLCM_RESIZE/DECIMACAO_BASE_24/FEATURES_M73_CM8b.txt", listGLCM, fmt='%.18e', delimiter=',',
+           newline='\n', header='',
+           footer='', comments='# ')
